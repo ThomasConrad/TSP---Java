3de64ab3a8ad5da00a41a08838d961ae9f0f4365
ThomasConrad <thomas.s.conrad@gmail.com>
2019-03-09 11:53:19 +0100

    Changed it so it doesn't save anything to a list. Should help with memory errors.

--- TSP.java
+++ TSP.java
@@ -1,14 +1,13 @@
-import  java.util.ArrayList;
 import java.util.*;
 import java.util.stream.*;
 
 public class TSP{
   static Random rand = new Random();
 
-  static int amount = 13;
-  static ArrayList<Double> pathLength = new ArrayList<Double>();
-  static ArrayList<int[]> paths = new ArrayList<int[]>();
+  static int amount = 10;
   static Double[][] lengths = new Double[amount][amount];
+  static int[] bestPath = new int[amount - 1];
+  static double bestLength;
 
   public static void main(String args[]){
     int[] combs = IntStream.rangeClosed(2, amount)
@@ -32,19 +31,15 @@ public class TSP{
     }
     for (int i = 0; i < amount;i++ ) {
       for (int j = i+1 ; j< amount; j++) {
-        System.out.print("(" + (i+1) + "," + (j+1) + ")");
-        System.out.format("%8.2f %s", lengths[i][j], " | ");
+        System.out.format("%s %2d %s %2d %s","(" , (i+1) , "," , (j+1) , ")");
+        System.out.format("%7.2f %s", lengths[i][j], " | ");
       }
       System.out.println();
     }
-    Double totaldist = 0.0;
-    for (int i = 1; i < amount; i++){
-      totaldist += lengths[0][i];
-    }
     // String[] comb = {"a", "b", "c"};
 
-
-    permute(combs, 0, n-1);
+    bestLength = calcLength(combs);
+    permute(combs, 0, n-1); //Create the permutations
     
     /* Here there be graphics */
 
@@ -53,9 +48,8 @@ public class TSP{
     StdDraw.setPenRadius(0.05/10);
     StdDraw.setPenColor(StdDraw.RED);
     int[] prev = coords[0];
-    int[] best = paths.get(pathLength.indexOf(Collections.min(pathLength)));
-    System.out.println(Collections.min(pathLength));
-    for (int i : best){
+    System.out.format("%f.2 \n",bestLength);
+    for (int i : bestPath){
       StdDraw.line(prev[0],prev[1],coords[i-1][0],coords[i-1][1]);
       prev = coords[i-1];
     }
@@ -71,7 +65,7 @@ public class TSP{
     StdDraw.setPenColor(StdDraw.BLUE);
     StdDraw.point(coords[0][0], coords[0][1]);
 
-    printArray(best);
+    printArray(bestPath);
 
 
     
@@ -80,8 +74,11 @@ public class TSP{
   static void permute(int[] input, int l, int r) {
 
     if (l == r){
-      pathLength.add(calcLength(input));
-      paths.add(input.clone());
+      if (calcLength(input) < bestLength) {
+        bestLength = calcLength(input);
+        bestPath = input.clone();
+      }
+
     }
     else
     {
